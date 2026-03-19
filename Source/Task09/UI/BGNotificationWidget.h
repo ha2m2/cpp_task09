4056317579eecc3baff0437b048ffc9b1521db37
ha2m2 <hamin0691@gmail.com>
2026-03-19 09:22:50 +0900

    add 공지 위젯

---
+++ Source/Task09/UI/BGNotificationWidget.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "BGNotificationWidget.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class TASK09_API UBGNotificationWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	void SetNotificationText(const FString& Message);
+
+protected:
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* NotificationText;
+};
